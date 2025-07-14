7267ce8a63acf69fdeb31782bf1a3f1e1cec1191
水水 <kdgzs2010@163.com>
2025-07-14 09:05:21 +0800

    debug

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+declare module 'xf-voice' {
+  interface XfVoiceOptions {
+    APPID: string;
+    APIKey: string;
+    APISecret: string;
+    onTextChange?: (text: string) => void;
+    onWillStatusChange?: (oldStatus: string, newStatus: string) => void;
+    language?: string;
+    accent?: string;
+    url?: string;
+    host?: string;
+  }
+
+  class XfVoiceDictation {
+    constructor(options: XfVoiceOptions);
+    start(): void;
+    stop(): void;
+    setStatus(status: string): void;
+    setResultText(params: { resultText?: string, resultTextTemp?: string }): void;
+    setParams(params: { language?: string, accent?: string }): void;
+  }
+
+  export = XfVoiceDictation;
+}
